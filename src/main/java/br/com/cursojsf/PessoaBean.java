0da29b34381d1739e0c09e43373966d787c2e5e1
wesley-saraiva <wesle@LAPTOP-26JSRI20>
2021-09-09 13:14:14 -0300

    projetojsf-commit

---
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -0,0 +1,62 @@
+package br.com.cursojsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+
+@ViewScoped
+@ManagedBean(name = "pessoaBean")
+
+public class PessoaBean {
+
+	private String nome;
+	
+	private HtmlCommandButton commandButton;
+	
+	private List<String> nomes = new ArrayList<String>();
+
+	public String addNome() {
+
+		nomes.add(nome);
+		
+		if (nomes.size() > 3) {
+			commandButton.setDisabled(true);
+			return "paginanavegada?faces-redirect=true";
+		}
+
+		return "";// null ou vazio faz ficar na mesma pagina
+	}
+
+	
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}
+	
+	
+
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+
+
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
